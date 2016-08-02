cc40be5e1084b7dceb1dc3af74f73ff262d135c0
Joaquim Puyo <joaquim.puyo@worldline.com>
2016-08-03 00:12:31 +0200

    day 4: added GameWorld and GameRenderer classes

---
+++ core/src/com/kilobolt/gameworld/GameRenderer.java
@@ -0,0 +1,16 @@
+package com.kilobolt.gameworld;
+
+import com.badlogic.gdx.Gdx;
+
+public class GameRenderer {
+
+    private GameWorld myWorld;
+
+    public GameRenderer(GameWorld world) {
+        myWorld = world;
+    }
+
+    public void render() {
+        Gdx.app.log("GameRenderer", "render");
+    }
+}
