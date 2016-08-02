7c5b3252b32b9845867aae2875a2297c67e3a432
Joaquim Puyo <joaquim.puyo@worldline.com>
2016-08-03 00:16:14 +0200

    day 4: added ortographic camera

--- core/src/com/kilobolt/gameworld/GameRenderer.java
+++ core/src/com/kilobolt/gameworld/GameRenderer.java
@@ -1,13 +1,17 @@
 package com.kilobolt.gameworld;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 
 public class GameRenderer {
 
     private GameWorld myWorld;
+    private OrthographicCamera cam;
 
     public GameRenderer(GameWorld world) {
         myWorld = world;
+        cam = new OrthographicCamera();
+        cam.setToOrtho(true, 136, 204);
     }
 
     public void render() {
